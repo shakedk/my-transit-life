3a6cc276a116c123e14fa6d5d8438370dd1fd5c6
Shaked Ofek <shakedk@gmail.com>
2023-07-01 16:23:46 +0300

    data later

--- Code for generating routes - patterns.py
+++ Code for generating routes - patterns.py
@@ -2,7 +2,7 @@
 import json
 routes = feed.routes
 # route_ids = feed.routes.route_id.tolist()
-route_ids = ['17440_900']
+route_ids = ['34447']
 for route_id in route_ids:
   geo_json = gk.routes_to_geojson(feed, [route_id])
   del geo_json['type']
@@ -12,3 +12,10 @@ for route_id in route_ids:
   geo_json['stops'] = stops[['stop_id','stop_name','stop_lon','stop_lat']].to_dict('records')
   with open('{}_{}_{}_test.json'.format(feed.agency.agency_name.iloc[0], feed.routes[routes.route_id == route_id].route_short_name.iloc[0], feed.routes[routes.route_id == route_id].route_id.iloc[0]), 'w', encoding='utf-8') as f:
     json.dump(geo_json, f, ensure_ascii=False, indent=4)
+
+
+
+    34445
+    אריה קוממיות
+    34447
+    מרכזית קוממיות
