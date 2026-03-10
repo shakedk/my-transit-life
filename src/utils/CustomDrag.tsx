bf858b18981202473e30890b9e5e7eb022710df9
Shaked Ofek <shaked.ofek@optibus.com>
2026-03-10 12:07:26 +0200

    Replace react-draggable with @dnd-kit and clean deps
    - Replace CustomDrag’s react-draggable implementation with @dnd-kit
    - Add @dnd-kit/core and @dnd-kit/utilities dependencies
    - Confirm localforage, moveable-helper, react-zoom-pan-pinch, and babel-preset-env are not present in package.json

--- src/utils/CustomDrag.tsx
+++ src/utils/CustomDrag.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/prop-types */
 import axios from "axios";
+import { DndContext, type DragEndEvent, useDraggable } from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
 import { getAuthAxios } from "../lib/api/apiClient";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import Draggable from "react-draggable";
 import { getPosterIDInDB } from "../lib/posters/utils";
 
 
 const CustomDrag = (props) => {
-  
   const [posterID, setPosterID] = useState(null);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
@@ -44,23 +44,68 @@ const CustomDrag = (props) => {
     getAuthAxios().put(`/api/poster/${posterID}`, params);
   };
 
-const handleStop = (event, dragElement) => {
-    setX(dragElement.x)
-    setY(dragElement.y)
-    customDraggableSaveData(props.id, dragElement.x, dragElement.y)
+  const DraggableContent = ({ children }) => {
+    const { attributes, listeners, setNodeRef, transform } = useDraggable({
+      id: props.id,
+      disabled: !props.isDraggable,
+    });
+
+    const style: React.CSSProperties = {
+      position: "absolute",
+      left: x,
+      top: y,
+      transform: transform ? CSS.Translate.toString(transform) : undefined,
+    };
+
+    const handlePointerDown: React.PointerEventHandler<HTMLDivElement> = (
+      event
+    ) => {
+      if (props.cancel && event.target instanceof Element) {
+        if (
+          event.target.matches(props.cancel) ||
+          event.target.closest(props.cancel)
+        ) {
+          return;
+        }
+      }
+      if (listeners.onPointerDown) {
+        listeners.onPointerDown(event);
+      }
+    };
+
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        {...attributes}
+        {...listeners}
+        onPointerDown={handlePointerDown}
+      >
+        {children}
+      </div>
+    );
   };
 
-return (
-  <Draggable
-    disabled={!props.isDraggable}
-    onStop={handleStop}
-    position={{ x: x, y: y }}
-    onMouseDown={props.onMouseDown}
-    cancel={props.cancel}
-  >
-    {props.children}
-  </Draggable>
-);
-}
+  const handleDragEnd = (event: DragEndEvent) => {
+    if (!props.isDraggable) return;
+    const { delta } = event;
+    if (!delta) return;
+    const newX = x + delta.x;
+    const newY = y + delta.y;
+    setX(newX);
+    setY(newY);
+    customDraggableSaveData(props.id, newX, newY);
+  };
+
+  if (!props.isDraggable) {
+    return <>{props.children}</>;
+  }
+
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      <DraggableContent>{props.children}</DraggableContent>
+    </DndContext>
+  );
+};
 
 export default CustomDrag;
